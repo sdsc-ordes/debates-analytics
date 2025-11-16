a4054c705c476cdd7f273ed0f480b38b6945bd54
sabinem <5292683+sabinem@users.noreply.github.com>
2025-11-16 08:24:31 +0100

    fix(frontend): disable cors to allow uploads to s3

--- components/frontend/svelte.config.js
+++ components/frontend/svelte.config.js
@@ -12,6 +12,10 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    // needed for uploading to S3 with presigned upload urls: in production minio would have to be configured instead
+    csrf: {
+        checkOrigin: false
+    }
   },
 }
 
