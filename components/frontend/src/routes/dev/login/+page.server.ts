9ce0c61a145ec4960356f3b195492a5617ccd729
sabinem <5292683+sabinem@users.noreply.github.com>
2026-01-30 22:22:04 +0100

    fix: redirect to dashboard for editor

--- components/frontend/src/routes/dev/login/+page.server.ts
+++ components/frontend/src/routes/dev/login/+page.server.ts
@@ -1,5 +1,6 @@
 import { redirect } from "@sveltejs/kit"
 import type { Actions } from "./$types"
+import { dev } from "$app/environment";
 
 export const actions: Actions = {
   // This handles the POST to /dev/login?/logout
@@ -16,7 +17,8 @@ export const actions: Actions = {
     cookies.set("session_id", "editor-secret", {
       path: "/",
       httpOnly: true,
-      secure: false,
+      sameSite: 'lax',
+      secure: !dev,
       maxAge: 60 * 60 * 24,
     })
     throw redirect(303, "/dashboard")
@@ -26,7 +28,8 @@ export const actions: Actions = {
     cookies.set("session_id", "reader-token", {
       path: "/",
       httpOnly: true,
-      secure: false,
+      sameSite: 'lax',
+      secure: !dev,
       maxAge: 60 * 60 * 24,
     })
     throw redirect(303, "/")
